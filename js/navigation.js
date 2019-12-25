4bb230e637da523e766512e0890a6d3ddf53ddc2
JonahDeleseleuc <55159668+Jonah-osf@users.noreply.github.com>
2019-12-24 23:07:53 -0500

    import

---
+++ js/navigation.js
@@ -0,0 +1,9 @@
+$('.drop').click (function(){
+    $(this).toggleClass('open');   
+});
+$('.drop-first').click (function(){
+    $(this).toggleClass('open');
+});
+
+new Date().getFullYear()
+document.getElementById("year").innerHTML = new Date().getFullYear();
